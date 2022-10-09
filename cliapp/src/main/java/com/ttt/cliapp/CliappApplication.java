59617aa6f17bdb03c86b3d0905df6ac4cd20b1b1
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-10-09 15:12:01 +0200

    First commit

---
+++ cliapp/src/main/java/com/ttt/cliapp/CliappApplication.java
@@ -0,0 +1,13 @@
+package com.ttt.cliapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliappApplication.class, args);
+	}
+
+}
