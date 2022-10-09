716fff4cbfe4bf9a0739102adec066014bfa703b
New Name Value <vladimir.shabanov@protonmail.com>
2022-10-09 15:12:01 +0200

    First commit

---
+++ cliapp/src/test/java/com/ttt/cliapp/CliappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttt.cliapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
