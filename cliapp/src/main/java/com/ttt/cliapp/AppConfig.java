62512b9a9c14ca3bcca659f41600d5af70b5e62a
New Name Value <vladimir.shabanov@protonmail.com>
2022-10-28 13:14:23 +0200

    Add Client and UpdateHandler into context

---
+++ cliapp/src/main/java/com/ttt/cliapp/AppConfig.java
@@ -0,0 +1,15 @@
+package com.ttt.cliapp;
+
+import lombok.NonNull;
+import org.drinkless.tdlib.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Client client(@NonNull UpdateHandler updateHandler) {
+        return Client.create(updateHandler, null, null);
+    }
+}
