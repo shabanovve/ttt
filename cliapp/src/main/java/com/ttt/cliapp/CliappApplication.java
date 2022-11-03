048136c3b0636e82a37b105e7ceadf2dc5c440d4
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-11-03 20:50:35 +0100

    Delete spring shell

--- cliapp/src/main/java/com/ttt/cliapp/CliappApplication.java
+++ cliapp/src/main/java/com/ttt/cliapp/CliappApplication.java
@@ -1,40 +1,15 @@
 package com.ttt.cliapp;
 
-import com.ttt.cliapp.config.ApiConfig;
-import com.ttt.cliapp.handler.LogMessageHandler;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.drinkless.tdlib.Client;
-import org.drinkless.tdlib.TdApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import javax.annotation.PostConstruct;
-import java.io.IOError;
-import java.io.IOException;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-@EnableConfigurationProperties(ApiConfig.class)
 public class CliappApplication {
-
-	@NonNull
-	private final LogMessageHandler logMessageHandler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CliappApplication.class, args);
 	}
 
-	@PostConstruct
-	private void init() {
-		// set log message handler to handle only fatal errors (0) and plain log messages (-1)
-		Client.setLogMessageHandler(0, logMessageHandler);
-
-		// disable TDLib log and redirect fatal errors and plain log messages to a file
-		Client.execute(new TdApi.SetLogVerbosityLevel(0));
-		if (Client.execute(new TdApi.SetLogStream(new TdApi.LogStreamFile("tdlib.log", 1 << 27, false))) instanceof TdApi.Error) {
-			throw new IOError(new IOException("Write access to the current directory is required"));
-		}
-	}
 }
--- cliapp/src/main/java/com/ttt/cliapp/handler/AbstractHandler.java
+++
@@ -1,6 +0,0 @@
-package com.ttt.cliapp.handler;
-
-@SuppressWarnings("unused")
-public abstract class AbstractHandler {
-    protected final String newLine = System.getProperty("line.separator");
-}
