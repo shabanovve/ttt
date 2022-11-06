69c7455b1eb519e83392384f9d301bf3914666ed
New Name Value <vladimir.shabanov@protonmail.com>
2022-11-06 16:13:11 +0100

    Add labels

--- app/src/main/java/com/ttt/app/AppApplication.java
+++ app/src/main/java/com/ttt/app/AppApplication.java
@@ -1,11 +1,9 @@
 package com.ttt.app;
 
 import javafx.application.Application;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class AppApplication {
 	public static void main(String[] args) {
 		Application.launch(AppFX.class);
--- app/src/main/java/com/ttt/app/StageInitializer.java
+++ app/src/main/java/com/ttt/app/StageInitializer.java
@@ -1,7 +1,11 @@
 package com.ttt.app;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -11,10 +15,21 @@ public class StageInitializer implements ApplicationListener<AppFX.StageReadyEve
     @Override
     public void onApplicationEvent(AppFX.StageReadyEvent event) {
         Stage stage = event.getStage();
-        BorderPane outerRoot = new BorderPane();
-        Scene scene = new Scene(outerRoot, 1024, 768);
+        VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        for (int i = 0; i < 100; i++) {
+            Label label = new Label("Start application.");
+            label.setFont(new Font("Arial", 20));
+            label.setMinHeight(50);
+            label.setMinWidth(100);
+            vbox.getChildren().addAll(label);
+        }
+        ScrollPane pane = new ScrollPane(vbox);
+        Scene scene = new Scene(pane, 1024, 768);
         stage.setScene(scene);
         stage.setTitle("LCC ttt app");
         stage.show();
     }
+
 }
