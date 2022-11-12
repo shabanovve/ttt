814cbdba07b1f550daef63bd016ff2de38c51981
New Name Value <vladimir.shabanov@protonmail.com>
2022-11-12 20:26:23 +0100

    Added ApiConfig

--- app/src/main/java/com/ttt/app/AppApplication.java
+++ app/src/main/java/com/ttt/app/AppApplication.java
@@ -1,8 +1,11 @@
 package com.ttt.app;
 
+import com.ttt.app.config.ApiConfig;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ApiConfig.class)
 @SpringBootApplication
 public class AppApplication {
 	public static void main(String[] args) {
