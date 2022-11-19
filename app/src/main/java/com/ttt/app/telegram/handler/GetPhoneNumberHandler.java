492fbfb5c9916c8c659115cf3908b79da904f13c
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-11-19 11:52:08 +0100

    Added AbstractStringDialogHandler

--- app/src/main/java/com/ttt/app/telegram/handler/GetPhoneNumberHandler.java
+++ app/src/main/java/com/ttt/app/telegram/handler/GetPhoneNumberHandler.java
@@ -2,33 +2,26 @@ package com.ttt.app.telegram.handler;
 
 import com.ttt.app.telegram.PhoneNumber;
 import com.ttt.app.telegram.event.GetPhoneNumberEvent;
-import com.ttt.app.view.StringDialog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicReference;
-
 
 @SuppressWarnings("unused")
 @RequiredArgsConstructor
 @Component
-public class GetPhoneNumberHandler implements ApplicationListener<GetPhoneNumberEvent> {
+public class GetPhoneNumberHandler extends AbstractStringDialogHandler
+        implements ApplicationListener<GetPhoneNumberEvent> {
     private final ConfigurableApplicationContext context;
+
     @Override
     public void onApplicationEvent(GetPhoneNumberEvent event) {
-        CountDownLatch latch = event.getLatch();
-        String title = "Enter phone number";
-        AtomicReference<String> result = new AtomicReference<>();
-        StringDialog.create(title, result);
-        context.getBeanFactory()
-                .registerSingleton(
-                        "phoneNumber",
-                        new PhoneNumber(result.get())
+        handle(
+                "Enter phone number", "phoneNumber", context,
+                result -> new PhoneNumber(result.get()),
+                event.getLatch()
                 );
-        latch.countDown();
     }
 
 }
