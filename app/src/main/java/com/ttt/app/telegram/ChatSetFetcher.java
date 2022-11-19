ead838f1d18969d93f5c1eae9ff205245c7a7cda
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-11-19 22:12:41 +0100

    Add ChatSetFetcher

---
+++ app/src/main/java/com/ttt/app/telegram/ChatSetFetcher.java
@@ -0,0 +1,47 @@
+package com.ttt.app.telegram;
+
+import com.ttt.app.view.event.MessageEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.drinkless.tdlib.Client;
+import org.drinkless.tdlib.TdApi;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+public class ChatSetFetcher {
+    private final ApplicationContext context;
+    private final ChatState chatState;
+
+
+    public synchronized void getMainChatSet(int limit) {
+        Client client = context.getBean(Client.class);
+        TdApi.LoadChats request = new TdApi.LoadChats(new TdApi.ChatListMain(), 0);
+        client.send(request, new Handler(limit));
+    }
+
+    @RequiredArgsConstructor
+    private class Handler implements Client.ResultHandler {
+        private final int limit;
+        @Override
+        public void onResult(TdApi.Object object) {
+            switch (object.getConstructor()) {
+                case TdApi.Error.CONSTRUCTOR -> {
+                    if (((TdApi.Error) object).code == 404) {
+                        synchronized (chatState.getChatSet()) {
+                            chatState.setHaveFullChatList(true);
+                        }
+                    } else {
+                        context.publishEvent(new MessageEvent(object.toString()));
+                        log.error(object.toString());
+                    }
+                }
+                case TdApi.Ok.CONSTRUCTOR -> getMainChatSet(limit);
+                default -> log.error("Cannot handle status " + object);
+            }
+        }
+    }
+
+}
