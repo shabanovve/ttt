ffdac11ff8c2caa4da011eaee87a79bc5e0df841
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-11-19 18:38:40 +0100

    Add config for logging

--- app/src/main/java/com/ttt/app/AppApplication.java
+++ app/src/main/java/com/ttt/app/AppApplication.java
@@ -1,11 +1,12 @@
 package com.ttt.app;
 
 import com.ttt.app.config.TelegramApiConfig;
+import com.ttt.app.config.TelegramLogConfig;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(TelegramApiConfig.class)
+@EnableConfigurationProperties({TelegramApiConfig.class, TelegramLogConfig.class})
 @SpringBootApplication
 public class AppApplication {
 	public static void main(String[] args) {
