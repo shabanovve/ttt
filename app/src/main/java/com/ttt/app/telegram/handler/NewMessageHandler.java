61fab6c86606b0565b3f3e2b2467501d75a8cdf0
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-11-24 21:37:56 +0100

    Add NewMessageHandler

---
+++ app/src/main/java/com/ttt/app/telegram/handler/NewMessageHandler.java
@@ -0,0 +1,25 @@
+package com.ttt.app.telegram.handler;
+
+import com.ttt.app.telegram.event.NewMessageEvent;
+import com.ttt.app.view.event.MessageEvent;
+import lombok.RequiredArgsConstructor;
+import org.drinkless.tdlib.TdApi;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class NewMessageHandler implements ApplicationListener<NewMessageEvent> {
+    private final ApplicationContext context;
+
+    @Override
+    public void onApplicationEvent(NewMessageEvent event) {
+        TdApi.UpdateNewMessage updateNewMessage = event.getUpdateNewMessage();
+        TdApi.MessageContent content = updateNewMessage.message.content;
+        if (content.getConstructor() == TdApi.MessageText.CONSTRUCTOR) {
+            String text = ((TdApi.MessageText)content).text.text;
+            context.publishEvent(new MessageEvent("Got message: " + text));
+        }
+    }
+}
