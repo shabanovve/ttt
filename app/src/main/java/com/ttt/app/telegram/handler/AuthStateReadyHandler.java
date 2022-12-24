cc055c4bac417a9181aa18446be7748b5b8d2d89
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-12-24 18:43:02 +0100

    Unbind AuthStateReadyHandler implementation

--- app/src/main/java/com/ttt/app/telegram/handler/AuthStateReadyHandler.java
+++ app/src/main/java/com/ttt/app/telegram/handler/AuthStateReadyHandler.java
@@ -1,26 +1,7 @@
 package com.ttt.app.telegram.handler;
 
-import com.ttt.app.telegram.ChatSetFetcher;
-import com.ttt.app.telegram.config.ChatConfig;
 import com.ttt.app.telegram.event.AuthStateReadyEvent;
-import com.ttt.app.telegram.event.MessageEvent;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
-@Log4j2
-@RequiredArgsConstructor
-@Component
-public class AuthStateReadyHandler implements ApplicationListener<AuthStateReadyEvent> {
-    private final ApplicationContext context;
-    private final ChatSetFetcher fetcher;
-    private final ChatConfig chatConfig;
-
-    @Override
-    public void onApplicationEvent(AuthStateReadyEvent event) {
-        context.publishEvent(new MessageEvent("Telegram connection is ready"));
-        fetcher.getMainChatSet(chatConfig.getLimit());
-    }
+public interface AuthStateReadyHandler extends ApplicationListener<AuthStateReadyEvent> {
 }
---
+++ app/src/main/java/com/ttt/app/telegram/handler/impl/AuthStateReadyHandlerImpl.java
@@ -0,0 +1,26 @@
+package com.ttt.app.telegram.handler.impl;
+
+import com.ttt.app.telegram.ChatSetFetcher;
+import com.ttt.app.telegram.config.ChatConfig;
+import com.ttt.app.telegram.event.AuthStateReadyEvent;
+import com.ttt.app.telegram.event.MessageEvent;
+import com.ttt.app.telegram.handler.AuthStateReadyHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@RequiredArgsConstructor
+@Component
+public class AuthStateReadyHandlerImpl implements AuthStateReadyHandler {
+    private final ApplicationContext context;
+    private final ChatSetFetcher fetcher;
+    private final ChatConfig chatConfig;
+
+    @Override
+    public void onApplicationEvent(AuthStateReadyEvent event) {
+        context.publishEvent(new MessageEvent("Telegram connection is ready"));
+        fetcher.getMainChatSet(chatConfig.getLimit());
+    }
+}
