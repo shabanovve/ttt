504c34c0209d42fce5b50e3bd144e4eeda1933f7
New Name Value <vladimir.shabanov@protonmail.com>
2022-12-24 18:57:27 +0100

    Unbind GetPhoneNumberHandler implementation

--- app/src/main/java/com/ttt/app/telegram/handler/GetPhoneNumberHandler.java
+++ app/src/main/java/com/ttt/app/telegram/handler/GetPhoneNumberHandler.java
@@ -1,26 +1,7 @@
 package com.ttt.app.telegram.handler;
 
-import com.ttt.app.telegram.AuthState;
 import com.ttt.app.telegram.event.GetPhoneNumberEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
-
-@SuppressWarnings("unused")
-@RequiredArgsConstructor
-@Component
-public class GetPhoneNumberHandler extends AbstractStringDialogHandler
-        implements ApplicationListener<GetPhoneNumberEvent> {
-    private final AuthState authState;
-
-    @Override
-    public void onApplicationEvent(GetPhoneNumberEvent event) {
-        handle(
-                "Enter phone number",
-                result -> authState.setPhoneNumber(result.get()),
-                event.getLatch()
-                );
-    }
-
+public interface GetPhoneNumberHandler extends ApplicationListener<GetPhoneNumberEvent> {
 }
