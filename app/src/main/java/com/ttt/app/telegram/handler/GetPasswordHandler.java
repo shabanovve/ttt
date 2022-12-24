0abc0d35d895ea2ab86e34e63f381614b30d044a
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-12-24 18:55:31 +0100

    Unbind GetPasswordHandler implementation

--- app/src/main/java/com/ttt/app/telegram/handler/GetPasswordHandler.java
+++ app/src/main/java/com/ttt/app/telegram/handler/GetPasswordHandler.java
@@ -1,25 +1,7 @@
 package com.ttt.app.telegram.handler;
 
-import com.ttt.app.telegram.AuthState;
 import com.ttt.app.telegram.event.GetPasswordEvent;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-@Component
-public class GetPasswordHandler extends AbstractStringDialogHandler
-        implements ApplicationListener<GetPasswordEvent> {
-    private final AuthState authState;
-
-
-    @Override
-    public void onApplicationEvent(GetPasswordEvent event) {
-        handle(
-                "Enter password",
-                result -> authState.setPassword(result.get()),
-                event.getLatch(),
-                true
-        );
-    }
+public interface GetPasswordHandler extends ApplicationListener<GetPasswordEvent> {
 }
---
+++ app/src/main/java/com/ttt/app/telegram/handler/impl/GetPasswordHandlerImpl.java
@@ -0,0 +1,26 @@
+package com.ttt.app.telegram.handler.impl;
+
+import com.ttt.app.telegram.AuthState;
+import com.ttt.app.telegram.event.GetPasswordEvent;
+import com.ttt.app.telegram.handler.AbstractStringDialogHandler;
+import com.ttt.app.telegram.handler.GetPasswordHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class GetPasswordHandlerImpl extends AbstractStringDialogHandler
+        implements GetPasswordHandler {
+    private final AuthState authState;
+
+
+    @Override
+    public void onApplicationEvent(GetPasswordEvent event) {
+        handle(
+                "Enter password",
+                result -> authState.setPassword(result.get()),
+                event.getLatch(),
+                true
+        );
+    }
+}
