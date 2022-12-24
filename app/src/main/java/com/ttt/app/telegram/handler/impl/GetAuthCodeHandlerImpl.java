551b283209cde6165557c09c3d68a0faada2a78e
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-12-24 18:53:17 +0100

    Unbind GetAuthCodeHandler implementation

---
+++ app/src/main/java/com/ttt/app/telegram/handler/impl/GetAuthCodeHandlerImpl.java
@@ -0,0 +1,24 @@
+package com.ttt.app.telegram.handler.impl;
+
+import com.ttt.app.telegram.AuthState;
+import com.ttt.app.telegram.event.GetAuthCodeEvent;
+import com.ttt.app.telegram.handler.AbstractStringDialogHandler;
+import com.ttt.app.telegram.handler.GetAuthCodeHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class GetAuthCodeHandlerImpl extends AbstractStringDialogHandler
+        implements GetAuthCodeHandler {
+    private final AuthState authState;
+
+    @Override
+    public void onApplicationEvent(GetAuthCodeEvent event) {
+        handle(
+                "Enter authorization code",
+                result -> authState.setAuthCode(result.get()),
+                event.getLatch()
+        );
+    }
+}
