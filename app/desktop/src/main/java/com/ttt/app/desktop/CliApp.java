4bc3cd2b3ce034e1dab0de30853c606dd77cf942
Vladimir Shabanov <vshabanov@griddynamics.com>
2022-12-25 20:22:15 +0100

    Create modules desktop and telegram-service

---
+++ app/desktop/src/main/java/com/ttt/app/desktop/CliApp.java
@@ -0,0 +1,10 @@
+package com.ttt.app.desktop;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class CliApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/telegram-service/src/main/java/com/ttt/telegram/Dummy.java
@@ -0,0 +1,10 @@
+package com.ttt.telegram;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Dummy {
+    public static void main(String[] args) {
+
+    }
+}
