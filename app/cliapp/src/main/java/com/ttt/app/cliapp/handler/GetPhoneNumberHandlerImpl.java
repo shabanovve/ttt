fa723ccbdb5c69d817c90e8fcb5c71c732771f27
Vladimir Shabanov <vshabanov@griddynamics.com>
2023-01-04 22:06:04 +0100

    Add read line handlers

---
+++ app/cliapp/src/main/java/com/ttt/app/cliapp/handler/GetPhoneNumberHandlerImpl.java
@@ -0,0 +1,26 @@
+package com.ttt.app.cliapp.handler;
+
+import com.ttt.app.telegram.AuthState;
+import com.ttt.app.telegram.event.GetPhoneNumberEvent;
+import com.ttt.app.telegram.handler.GetPhoneNumberHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+@SuppressWarnings("unused")
+@RequiredArgsConstructor
+@Component
+public class GetPhoneNumberHandlerImpl extends AbstractStringDialogHandler
+        implements GetPhoneNumberHandler {
+    private final AuthState authState;
+
+    @Override
+    public void onApplicationEvent(GetPhoneNumberEvent event) {
+        handle(
+                "Enter phone number",
+                result -> authState.setPhoneNumber(result.get()),
+                event.getLatch()
+                );
+    }
+
+}
