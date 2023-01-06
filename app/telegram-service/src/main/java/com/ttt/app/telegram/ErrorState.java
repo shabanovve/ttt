2da847c249c2940aeb7064aa80ef7104c6f7286f
New Name Value <vladimir.shabanov@protonmail.com>
2023-01-06 20:59:46 +0100

    Improve error breaker

--- app/telegram-service/src/main/java/com/ttt/app/telegram/ErrorState.java
+++ app/telegram-service/src/main/java/com/ttt/app/telegram/ErrorState.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.drinkless.tdlib.TdApi;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,16 +16,32 @@ import java.util.Map;
 @Component
 public class ErrorState {
     private static final Integer MAX_COUNT_ERROR = 10;
-    private static final Map<TdApi.Error, Integer> errorCounter = new HashMap<>();
+    private static final Map<Integer, Integer> errorCounter = new HashMap<>();
+    private Instant before = Instant.now();
+
     public void countError(TdApi.Error error) {
-        Integer count = errorCounter.get(error);
+        Integer count = errorCounter.get(error.code);
         if (count == null) {
-            errorCounter.put(error, 1);
+            errorCounter.put(error.code, 1);
         } else {
-            if (count > MAX_COUNT_ERROR) {
-                throw new IllegalStateException("Too many error " + error);
+            if (Instant.now().minus(1, ChronoUnit.MINUTES).isAfter(before)) {
+                errorCounter.clear();
+                before = Instant.now();
+                count = 0;
             }
-            errorCounter.put(error, ++count);
+            if (count > MAX_COUNT_ERROR) {
+                throw new IllegalStateException("Too many error " + errorCounter);
+            }
+            pause(count);
+            errorCounter.put(error.code, ++count);
+        }
+    }
+
+    private void pause(Integer count) {
+        try {
+            Thread.sleep(count * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
